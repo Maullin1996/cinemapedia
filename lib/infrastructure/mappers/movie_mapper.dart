3c430687bf4cde5023985f5c8ecd1db7d5d08365
Mauricio Llanos <llanosmauricio10@gmail.com>
2024-10-19 11:17:51 -0500

    Search delegate and stream builder finished

--- lib/infrastructure/mappers/movie_mapper.dart
+++ lib/infrastructure/mappers/movie_mapper.dart
@@ -16,7 +16,7 @@ class MovieMapper {
       popularity: moviedb.popularity,
       posterPath: (moviedb.posterPath != '')
           ? 'https://image.tmdb.org/t/p/w500${moviedb.posterPath}'
-          : 'no-poster',
+          : 'https://cdn.movieguide.org/wp-content/uploads/2021/09/Poster-Not-Available.jpg',
       releaseDate: moviedb.releaseDate,
       title: moviedb.title,
       video: moviedb.video,
--- lib/presentation/widgets/shared/custom_appbar.dart
+++ lib/presentation/widgets/shared/custom_appbar.dart
@@ -1,10 +1,16 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-class CustomAppbar extends StatelessWidget {
+import 'package:cinemapedia/domain/entities/movie.dart';
+import 'package:cinemapedia/presentation/delegates/search_movie_delegate.dart';
+import 'package:cinemapedia/presentation/providers/movies/movies_repository_provider.dart';
+import 'package:go_router/go_router.dart';
+
+class CustomAppbar extends ConsumerWidget {
   const CustomAppbar({super.key});
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
     final colors = Theme.of(context).colorScheme;
     final titleStyle = Theme.of(context).textTheme.titleMedium;
     return SafeArea(
@@ -18,7 +24,19 @@ class CustomAppbar extends StatelessWidget {
               const SizedBox(width: 5),
               Text('CinemaPedia', style: titleStyle),
               const Spacer(),
-              IconButton(onPressed: () {}, icon: const Icon(Icons.search))
+              IconButton(
+                  onPressed: () {
+                    final movieRepository = ref.read(movieRepositoryProvider);
+                    showSearch<Movie?>(
+                        context: context,
+                        delegate: SearchMovieDelegate(
+                          searchMovies: movieRepository.searchMovies,
+                        )).then((movie) {
+                      if (movie == null) return;
+                      context.push('/movie/${movie.id}');
+                    });
+                  },
+                  icon: const Icon(Icons.search))
             ],
           ),
         ),
